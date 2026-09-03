ed7428316a1c097cf991acd8f0f54a4a8a65d73f
PatrickWacholtz04 <157766166+PatrickWacholtz04@users.noreply.github.com>
2026-09-03 21:41:06 +0000

    Implement basic 3d rendering of a cube.

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -1,299 +1,295 @@
-use crossterm::{
-    cursor,
-    queue,
-    style::{
-        Color,
-        Print,
-        ResetColor,
-        SetBackgroundColor,
-        SetForegroundColor,
-    },
-    terminal::{
-        self,
-        EnterAlternateScreen,
-        LeaveAlternateScreen,
-    },
-    ExecutableCommand,
+use std::io::Result;
+use std::{thread, time::Duration, time::Instant};
+
+use crate::renderer_2d::{
+    Point,
+    RGB
 };
 
-use std::io::{Result, Stdout, Write, stdout};
-use std::{thread, time::Duration};
+mod renderer_2d;
 
-
-const OUT_W:usize = 100;
-const OUT_H:usize = 100;
-
-#[derive(Default, Clone, Copy, Debug)]
-struct RGB {
-    r: u8,
-    g: u8,
-    b: u8,
+#[derive(Default, Clone, Copy)]
+struct Vec3d {
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
-impl RGB {
-    fn to_crossterm(self) -> Color {
-        Color::Rgb {
-            r: self.r,
-            g: self.g,
-            b: self.b,
-        }
-    }
+#[derive(Default, Clone, Copy)]
+struct Triangle {
+    p: [Vec3d; 3],
 }
 
-#[derive(Default, Clone, Copy, Debug)]
-struct Point {
-    x: usize,
-    y: usize,
+#[derive(Default, Clone)]
+struct Mesh {
+    tris: Vec<Triangle>,
+}
+
+#[derive(Default, Clone, Copy)]
+struct Mat4x4 {
+    m: [[f32; 4]; 4],
 }
 
 struct Renderer {
-    out_w: usize,
-    out_h: usize,
-    screen_buffer: Vec<Vec<RGB>>,
-    draw_pixel: char,
-    stdout: Stdout,
+    mesh_cube: Mesh,
+    mat_proj: Mat4x4,
 }
 
 impl Renderer {
     fn new() -> Self {
-        // Create a screen buffer the size of the output containing an RGB value for each pixel
-        Self {
-            out_w: OUT_W,
-            out_h: OUT_H,
-            screen_buffer: vec![vec![RGB::default(); OUT_H]; OUT_W],
-            draw_pixel: '▄',
-            stdout: stdout(),
+        let mesh_cube = Mesh {
+            tris: vec![
+                // Front face
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 0.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 0.0 },
+                    ],
+                },
+
+                // Back face
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 1.0 },
+                    ],
+                },
+
+                // Left face
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 0.0, y: 0.0, z: 1.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 1.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 1.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 0.0 },
+                    ],
+                },
+
+                // Right face
+                Triangle {
+                    p: [
+                        Vec3d { x: 1.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 1.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 1.0 },
+                    ],
+                },
+
+                // Top face
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 1.0, z: 0.0 },
+                        Vec3d { x: 0.0, y: 1.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 1.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 1.0 },
+                        Vec3d { x: 1.0, y: 1.0, z: 0.0 },
+                    ],
+                },
+
+                // Bottom face
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 1.0 },
+                    ],
+                },
+                Triangle {
+                    p: [
+                        Vec3d { x: 0.0, y: 0.0, z: 0.0 },
+                        Vec3d { x: 1.0, y: 0.0, z: 1.0 },
+                        Vec3d { x: 0.0, y: 0.0, z: 1.0 },
+                    ],
+                },
+            ],
+        };
+
+        let mat_proj = Mat4x4::default();
+
+        Self { 
+            mesh_cube,
+            mat_proj,
         }
-        
+
     }
 
-    fn set_pixel(&mut self, point: Point, color: RGB) {
-        self.screen_buffer[point.x][point.y] = color;
-    }
+    fn multiply_matrix_vector(i: Vec3d, m: Mat4x4) -> Vec3d {
+        let mut o = Vec3d::default();
 
-    fn draw_line(&mut self, point0: Point, point1: Point, color: RGB) {
-        // Generate line using Bresenham's Line Algorithm
-            // https://www.youtube.com/watch?v=CceepU1vIKo
+        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+        let w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
 
-        let x0 = point0.x;
-        let x1 = point1.x;
-        let y0 = point0.y;
-        let y1 = point1.y;
-
-        let dx = x1 as isize - x0 as isize;
-        let dy = y1 as isize - y0 as isize;
-
-        if dx.abs() > dy.abs() {
-            self.draw_line_horizontal(point0, point1, color);
-        }
-        else {
-            self.draw_line_vertical(point0, point1, color);
-        }
-    }
-
-    fn draw_line_horizontal(&mut self, point0: Point, point1: Point, color: RGB) {
-        // Generate line using Bresenham's Line Algorithm
-            // https://www.youtube.com/watch?v=CceepU1vIKo
-
-        let mut x0 = point0.x;
-        let mut x1 = point1.x;
-        let mut y0 = point0.y;
-        let mut y1 = point1.y;
-
-        if x0 > x1 {
-            (x0, x1) = (x1, x0);
-            (y0, y1) = (y1, y0);
+        if w != 0.0 {
+            o.x /= w;
+            o.y /= w;
+            o.z /= w;
         }
 
-        let dx = x1 as isize - x0 as isize;
-        let mut dy = y1 as isize - y0 as isize;
-
-        let dir = if dy < 0 {-1} else {1};
-        dy *= dir;
-
-        if dx != 0 {
-            let mut y = y0 as isize;
-            let mut p = 2*dy - dx;
-            for i in 0..(dx+1) {
-                
-                self.set_pixel(Point{x: x0 + i as usize, y: y as usize}, color);
-
-                if p >= 0 {
-                    y += dir;
-                    p = p - 2*dx;
-                }
-                p = p + 2*dy;
-            }
-        }
-    }
-
-    fn draw_line_vertical(&mut self, point0: Point, point1: Point, color: RGB) {
-        // Generate line using Bresenham's Line Algorithm
-            // https://www.youtube.com/watch?v=CceepU1vIKo
-
-        let mut x0 = point0.x;
-        let mut x1 = point1.x;
-        let mut y0 = point0.y;
-        let mut y1 = point1.y;
-
-        if y0 > y1 {
-            (x0, x1) = (x1, x0);
-            (y0, y1) = (y1, y0);
-        }
-
-        let mut dx = x1 as isize - x0 as isize;
-        let dy = y1 as isize - y0 as isize;
-
-        let dir = if dx < 0 {-1} else {1};
-        dx *= dir;
-
-        if dy != 0 {
-            let mut x = x0 as isize;
-            let mut p = 2*dx - dy;
-            for i in 0..(dy+1) {
-                self.set_pixel(Point{x: x as usize, y: y0 + i as usize}, color);
-
-                if p >= 0 {
-                    x += dir;
-                    p = p - 2*dy;
-                }
-                p = p + 2*dx;
-            }
-        }
-    }
-
-    fn draw_triangle(&mut self, point0: Point, point1: Point, point2: Point, color: RGB) {
-        // Draw lines connecting points into a triangle
-        self.draw_line(point0, point1, color);
-        self.draw_line(point1, point2, color);
-        self.draw_line(point2, point0, color);
-    }
-
-    fn render_crossterm(&mut self) -> Result<()> {     
-        // Reset cursor position to top left instead of letting terminal scroll
-        queue!(
-            self.stdout,
-            cursor::MoveTo(0, 0),
-        )?;
-
-        // Loop through screen buffer
-        for y in ( 0..self.out_h).step_by(2) {
-            for x in 0..self.out_w {
-                let rgb_top = self.screen_buffer[x][y];
-                let rgb_btm = 
-                    if y + 1 < self.out_h { self.screen_buffer[x][y+1] }
-                    else { RGB::default() };
-
-                // Add draw pixel to queue with color
-                queue!(
-                    self.stdout,
-                    SetForegroundColor(rgb_btm.to_crossterm()),
-                    SetBackgroundColor(rgb_top.to_crossterm()),
-                    Print(self.draw_pixel),
-                    ResetColor,
-                )?;
-            }
-            // Move cursor to start of next line
-            queue!(
-                self.stdout,
-                cursor::MoveTo(0, (y / 2 + 1) as u16),
-            )?;
-        }
-
-        // Clear color settings
-        queue!(
-            self.stdout,
-            ResetColor,
-        )?;
-        // Flush queue to screen
-        self.stdout.flush()?;
-        Ok(())
-    }
-
-
-    fn render(&self) {
-        for y in ( 0..self.out_h).step_by(2) {
-            for x in 0..self.out_w {
-                let rgb_top = self.screen_buffer[x][y];
-                let rgb_btm = 
-                    if y + 1 < self.out_h { self.screen_buffer[x][y+1] }
-                    else { RGB::default() }; 
-
-                // Background Color (Top Pixel)
-                // Foreground Color (Bottom Pixel)
-                // Pixel Character
-                // Reset Color(s)
-                print!("\x1b[48;2;{};{};{}m\
-                        \x1b[38;2;{};{};{}m\
-                        {}\
-                        \x1b[0m", 
-                        rgb_top.r, rgb_top.g, rgb_top.b, 
-                        rgb_btm.r, rgb_btm.g, rgb_btm.b, 
-                        self.draw_pixel
-                    );
-            }
-            println!();
-        }
+        return o;
     }
 
 }
+
+
 
 
 fn main() -> Result<()> {
+    let mut renderer_2d = renderer_2d::Renderer::new();
+    let mut input_handler = renderer_2d::InputHandler::new();
+    
+
     let mut renderer = Renderer::new();
 
-    renderer.stdout.execute(EnterAlternateScreen)?;
-    terminal::enable_raw_mode()?;
-    renderer.stdout.execute(cursor::Hide)?;
+    // Projection Matrix
+    let fNear = 0.1f32;
+    let fFar = 1000.0f32;
+    let fFov = 90.0f32;
+    let fAspectRatio = renderer_2d.out_h as f32 / renderer_2d.out_w as f32;
+    let fFovRad = 1.0f32 / (fFov * 0.5 / 180.0 * 3.14159).tan();
 
-    let result = run(&mut renderer);
-
-    // Always restore terminal state.
-    renderer.stdout.execute(cursor::Show)?;
-    terminal::disable_raw_mode()?;
-    renderer.stdout.execute(LeaveAlternateScreen)?;
-
-    result
-}
+    renderer.mat_proj.m[0][0] = fAspectRatio * fFovRad;
+    renderer.mat_proj.m[1][1] = fFovRad;
+    renderer.mat_proj.m[2][2] = fFar / (fFar - fNear);
+    renderer.mat_proj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+    renderer.mat_proj.m[2][3] = 1.0f32;
+    renderer.mat_proj.m[3][3] = 0.0f32;
 
 
-fn run(renderer: &mut Renderer) -> Result<()> {
-    let mut frame_counter = 0;
+    let _ = renderer_2d::ready_terminal(&mut renderer_2d);
+
+    let start_time = Instant::now();
+    let target_frame_time = Duration::from_secs_f32(1.0 / renderer_2d.target_framerate as f32);
 
     loop {
-        frame_counter += 1;
+        let frame_start = Instant::now();
+        let elapsed_time = frame_start - start_time;
+
+        let _ = input_handler.update();
+        if input_handler.exit {
+            break;
+        }
 
         // Clear the logical framebuffer.
-        renderer.screen_buffer.fill(
-            vec![RGB::default(); renderer.out_h]
+        renderer_2d.screen_buffer.fill(
+            vec![RGB::default(); renderer_2d.out_h]
         );
+        
 
-        renderer.draw_triangle(
-            Point { x: 5, y: 5 },
-            Point { x: 22, y: 13 },
-            Point { x: 5, y: 20 },
-            RGB {
-                r: 0,
-                g: 0,
-                b: 255,
-            },
-        );
+        // Draw Logic
+        let mut mat_rot_z = Mat4x4::default();
+        let mut mat_rot_x = Mat4x4::default();
 
-        renderer.draw_triangle(
-            Point { x: 5, y: 5 },
-            Point { x: 22, y: 13 },
-            Point { x: 14, y: 1 },
-            RGB {
-                r: 255,
-                g: 0,
-                b: 0,
-            },
-        );
+        let f_theta = elapsed_time.as_secs_f32();
 
-        renderer.render_crossterm()?;
+        // Rotation Z
+        mat_rot_z.m[0][0] = f_theta.cos();
+        mat_rot_z.m[0][1] = f_theta.sin();
+        mat_rot_z.m[1][0] = -f_theta.sin();
+        mat_rot_z.m[1][1] = f_theta.cos();
+        mat_rot_z.m[2][2] = 1.0;
+        mat_rot_z.m[3][3] = 1.0;
 
-        thread::sleep(Duration::from_millis(16));
+        // Rotation X
+        mat_rot_x.m[0][0] = 1.0;
+        mat_rot_x.m[1][1] = (0.5 * f_theta).cos();
+        mat_rot_x.m[1][2] = (0.5 * f_theta).sin();
+        mat_rot_x.m[2][1] = -(0.5 * f_theta).sin();
+        mat_rot_x.m[2][2] = (0.5 * f_theta).cos();
+        mat_rot_x.m[3][3] = 1.0;
+
+        for tri in &renderer.mesh_cube.tris {
+            let mut projected = Triangle::default();
+            let mut rotated_z = Triangle::default();
+            let mut rotated_zx = Triangle::default();
+
+
+            rotated_z.p[0] = Renderer::multiply_matrix_vector(tri.p[0], mat_rot_z);
+            rotated_z.p[1] = Renderer::multiply_matrix_vector(tri.p[1], mat_rot_z);
+            rotated_z.p[2] = Renderer::multiply_matrix_vector(tri.p[2], mat_rot_z);
+
+            rotated_zx.p[0] = Renderer::multiply_matrix_vector(rotated_z.p[0], mat_rot_x);
+            rotated_zx.p[1] = Renderer::multiply_matrix_vector(rotated_z.p[1], mat_rot_x);
+            rotated_zx.p[2] = Renderer::multiply_matrix_vector(rotated_z.p[2], mat_rot_x);
+
+            let mut translated = rotated_zx;
+            translated.p[0].z = rotated_zx.p[0].z + 3.0;
+            translated.p[1].z = rotated_zx.p[1].z + 3.0;
+            translated.p[2].z = rotated_zx.p[2].z + 3.0;
+            
+            projected.p[0] = Renderer::multiply_matrix_vector(translated.p[0], renderer.mat_proj);
+            projected.p[1] = Renderer::multiply_matrix_vector(translated.p[1], renderer.mat_proj);
+            projected.p[2] = Renderer::multiply_matrix_vector(translated.p[2], renderer.mat_proj);
+
+            // Scale into view
+            projected.p[0].x += 1.0;    projected.p[0].y += 1.0;
+            projected.p[1].x += 1.0;    projected.p[1].y += 1.0;
+            projected.p[2].x += 1.0;    projected.p[2].y += 1.0;
+
+            projected.p[0].x *= 0.5 * renderer_2d.out_w as f32;
+            projected.p[0].y *= 0.5 * renderer_2d.out_h as f32;
+            projected.p[1].x *= 0.5 * renderer_2d.out_w as f32;
+            projected.p[1].y *= 0.5 * renderer_2d.out_h as f32;
+            projected.p[2].x *= 0.5 * renderer_2d.out_w as f32;
+            projected.p[2].y *= 0.5 * renderer_2d.out_h as f32;
+
+            renderer_2d.draw_triangle(Point{x: projected.p[0].x as usize, y: projected.p[0].y as usize}, 
+                                    Point{x: projected.p[1].x as usize, y: projected.p[1].y as usize},
+                                    Point{x: projected.p[2].x as usize, y: projected.p[2].y as usize},
+                                    RGB{r: 255, g: 255, b:255}
+            );
+
+        }
+
+        renderer_2d.render()?;
+
+        let frame_time = frame_start.elapsed();
+        if frame_time < target_frame_time {
+            thread::sleep(target_frame_time - frame_time);
+        }
+
     }
 
 
+    let result = renderer_2d::run(&mut renderer_2d, &mut input_handler);
+
+
+    
+    let _ = renderer_2d::restore_terminal(&mut renderer_2d);
+
+    result
 }
---
+++ renderer/src/renderer_2d.rs
@@ -0,0 +1,313 @@
+use crossterm::{
+    cursor,
+    queue,
+    style::{
+        Color,
+        Print,
+        ResetColor,
+        SetBackgroundColor,
+        SetForegroundColor,
+    },
+    event::{
+        self,
+        Event,
+        KeyCode,
+        KeyEvent,
+    },
+    terminal::{
+        self,
+        EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
+};
+
+use std::io::{Result, Stdout, Write, stdout};
+use std::{thread, time::Duration};
+
+
+const OUT_W:usize = 200;
+const OUT_H:usize = 100;
+
+#[derive(Default, Clone, Copy, Debug)]
+pub struct RGB {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl RGB {
+    fn to_crossterm(self) -> Color {
+        Color::Rgb {
+            r: self.r,
+            g: self.g,
+            b: self.b,
+        }
+    }
+}
+
+#[derive(Default, Clone, Copy, Debug)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Renderer {
+    pub out_w: usize,
+    pub out_h: usize,
+    pub screen_buffer: Vec<Vec<RGB>>,
+    draw_pixel: char,
+    stdout: Stdout,
+    pub target_framerate: u16,
+}
+
+pub struct InputHandler {
+    pub exit: bool,
+}
+
+impl InputHandler {
+    pub fn new() -> Self {
+        Self {
+            exit: false,
+        }
+    }
+
+    pub fn update(&mut self) -> Result<()> {
+        while event::poll(Duration::ZERO)? {
+            if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+                match code {
+                    KeyCode::Esc => {
+                        self.exit = true;
+                    }
+                    _ => {}
+                }
+            }
+
+
+        }
+
+        Ok(())
+    }
+
+}
+
+impl Renderer {
+    pub fn new() -> Self {
+        // Create a screen buffer the size of the output containing an RGB value for each pixel
+        Self {
+            out_w: OUT_W,
+            out_h: OUT_H,
+            screen_buffer: vec![vec![RGB::default(); OUT_H]; OUT_W],
+            draw_pixel: '▄',
+            stdout: stdout(),
+            target_framerate: 60,
+        }
+        
+    }
+
+    fn set_pixel(&mut self, point: Point, color: RGB) {
+        if point.x < self.out_w && point.y < self.out_h {
+            self.screen_buffer[point.x][point.y] = color;
+        }
+    }
+
+    fn draw_line(&mut self, point0: Point, point1: Point, color: RGB) {
+        // Generate line using Bresenham's Line Algorithm
+            // https://www.youtube.com/watch?v=CceepU1vIKo
+
+        let x0 = point0.x;
+        let x1 = point1.x;
+        let y0 = point0.y;
+        let y1 = point1.y;
+
+        let dx = x1 as isize - x0 as isize;
+        let dy = y1 as isize - y0 as isize;
+
+        if dx.abs() > dy.abs() {
+            self.draw_line_horizontal(point0, point1, color);
+        }
+        else {
+            self.draw_line_vertical(point0, point1, color);
+        }
+    }
+
+    fn draw_line_horizontal(&mut self, point0: Point, point1: Point, color: RGB) {
+        // Generate line using Bresenham's Line Algorithm
+            // https://www.youtube.com/watch?v=CceepU1vIKo
+
+        let mut x0 = point0.x;
+        let mut x1 = point1.x;
+        let mut y0 = point0.y;
+        let mut y1 = point1.y;
+
+        if x0 > x1 {
+            (x0, x1) = (x1, x0);
+            (y0, y1) = (y1, y0);
+        }
+
+        let dx = x1 as isize - x0 as isize;
+        let mut dy = y1 as isize - y0 as isize;
+
+        let dir = if dy < 0 {-1} else {1};
+        dy *= dir;
+
+        if dx != 0 {
+            let mut y = y0 as isize;
+            let mut p = 2*dy - dx;
+            for i in 0..(dx+1) {
+                
+                self.set_pixel(Point{x: x0 + i as usize, y: y as usize}, color);
+
+                if p >= 0 {
+                    y += dir;
+                    p = p - 2*dx;
+                }
+                p = p + 2*dy;
+            }
+        }
+    }
+
+    fn draw_line_vertical(&mut self, point0: Point, point1: Point, color: RGB) {
+        // Generate line using Bresenham's Line Algorithm
+            // https://www.youtube.com/watch?v=CceepU1vIKo
+
+        let mut x0 = point0.x;
+        let mut x1 = point1.x;
+        let mut y0 = point0.y;
+        let mut y1 = point1.y;
+
+        if y0 > y1 {
+            (x0, x1) = (x1, x0);
+            (y0, y1) = (y1, y0);
+        }
+
+        let mut dx = x1 as isize - x0 as isize;
+        let dy = y1 as isize - y0 as isize;
+
+        let dir = if dx < 0 {-1} else {1};
+        dx *= dir;
+
+        if dy != 0 {
+            let mut x = x0 as isize;
+            let mut p = 2*dx - dy;
+            for i in 0..(dy+1) {
+                self.set_pixel(Point{x: x as usize, y: y0 + i as usize}, color);
+
+                if p >= 0 {
+                    x += dir;
+                    p = p - 2*dy;
+                }
+                p = p + 2*dx;
+            }
+        }
+    }
+
+    pub fn draw_triangle(&mut self, point0: Point, point1: Point, point2: Point, color: RGB) {
+        // Draw lines connecting points into a triangle
+        self.draw_line(point0, point1, color);
+        self.draw_line(point1, point2, color);
+        self.draw_line(point2, point0, color);
+    }
+
+    pub fn render(&mut self) -> Result<()> {     
+        // Reset cursor position to top left instead of letting terminal scroll
+        queue!(
+            self.stdout,
+            cursor::MoveTo(0, 0),
+        )?;
+
+        // Loop through screen buffer
+        for y in ( 0..self.out_h).step_by(2) {
+            for x in 0..self.out_w {
+                let rgb_top = self.screen_buffer[x][y];
+                let rgb_btm = 
+                    if y + 1 < self.out_h { self.screen_buffer[x][y+1] }
+                    else { RGB::default() };
+
+                // Add draw pixel to queue with color
+                queue!(
+                    self.stdout,
+                    SetForegroundColor(rgb_btm.to_crossterm()),
+                    SetBackgroundColor(rgb_top.to_crossterm()),
+                    Print(self.draw_pixel),
+                    ResetColor,
+                )?;
+            }
+            // Move cursor to start of next line
+            queue!(
+                self.stdout,
+                cursor::MoveTo(0, (y / 2 + 1) as u16),
+            )?;
+        }
+
+        // Clear color settings
+        queue!(
+            self.stdout,
+            ResetColor,
+        )?;
+        // Flush queue to screen
+        self.stdout.flush()?;
+        Ok(())
+    }
+
+}
+
+pub fn ready_terminal(renderer: &mut Renderer) -> Result<()> {
+    renderer.stdout.execute(EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+    renderer.stdout.execute(cursor::Hide)?;
+    Ok(())
+}
+
+pub fn restore_terminal(renderer: &mut Renderer) -> Result<()>  {
+    renderer.stdout.execute(cursor::Show)?;
+    terminal::disable_raw_mode()?;
+    renderer.stdout.execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+pub fn run(renderer: &mut Renderer, input_handler: &mut InputHandler) -> Result<()> {
+    let _ = ready_terminal(renderer);
+
+    loop {
+        let _ = input_handler.update();
+        if input_handler.exit {break}
+
+
+        // Clear the logical framebuffer.
+        renderer.screen_buffer.fill(
+            vec![RGB::default(); renderer.out_h]
+        );
+
+        renderer.draw_triangle(
+            Point { x: 5, y: 5 },
+            Point { x: 22, y: 13 },
+            Point { x: 5, y: 20 },
+            RGB {
+                r: 0,
+                g: 0,
+                b: 255,
+            },
+        );
+
+        renderer.draw_triangle(
+            Point { x: 5, y: 5 },
+            Point { x: 22, y: 13 },
+            Point { x: 14, y: 1 },
+            RGB {
+                r: 255,
+                g: 0,
+                b: 0,
+            },
+        );
+
+        renderer.render()?;
+
+        thread::sleep(Duration::from_millis( (1000/renderer.target_framerate) as u64));
+    }
+
+    // Restore terminal state.
+    let _ = restore_terminal(renderer);
+
+    Ok(())
+}
